be275f707870097f46e25d62a64d7fe71f855e76
Тимур Душанов <engineer4dept@yandex.ru>
2022-08-17 11:46:36 +0300

    изменение кода на основании замечания преподавателя

--- geo-service/src/test/java/ru/netology/sender/MessageSenderTest.java
+++ geo-service/src/test/java/ru/netology/sender/MessageSenderTest.java
@@ -1,3 +1,5 @@
+package ru.netology.sender;
+
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -52,38 +54,6 @@ public class MessageSenderTest {
 
     }
 
-    @ParameterizedTest
-    @MethodSource("source")
-    public void GeoServiceImplTest(String ip, Location expected) {
-        Location location = geoServiceImpl.byIp(ip);
-        assertEquals(expected.getCity(), location.getCity());
-        assertEquals(expected.getCountry(), location.getCountry());
-        assertEquals(expected.getStreet(), location.getStreet());
-        assertEquals(expected.getBuiling(), location.getBuiling());
-
-    }
-
-    private static Stream<Arguments> source() {
-        return Stream.of(Arguments.of("127.0.0.1", new Location(null, null, null, 0)),
-                Arguments.of("172.0.32.11", new Location("Moscow", Country.RUSSIA, "Lenina", 15)),
-                Arguments.of("96.44.183.149", new Location("New York", Country.USA, " 10th Avenue", 32)));
-    }
-
-    @ParameterizedTest
-    @MethodSource("source2")
-    public void localizationTest(Country country, String hi) {
-        LocalizationServiceImpl localizationService = new LocalizationServiceImpl();
-        String result = localizationService.locale(country);
-        assertEquals(hi, result);
-
-    }
-
-    private static Stream<Arguments> source2() {
-        return Stream.of(Arguments.of(Country.USA, "Welcome"),
-                Arguments.of(Country.RUSSIA, "Добро пожаловать"),
-                Arguments.of(Country.BRAZIL, "Welcome"),
-                Arguments.of(Country.GERMANY, "Welcome"));
-    }
 
     @Test
     public void messageSenderRussianScenarioTest() {
